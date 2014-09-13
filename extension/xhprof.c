0093e49563148dd13c248d08cb1268193f5be466
Benjamin Eberlei <kontakt@beberlei.de>
2014-09-13 10:20:10 +0200

    Update CHANGELOG

--- extension/xhprof.c
+++ extension/xhprof.c
@@ -83,7 +83,7 @@
  */
 
 /* XHProf version                           */
-#define XHPROF_VERSION       "0.9.7"
+#define XHPROF_VERSION       "0.9.8"
 
 /* Fictitious function name to represent top of the call tree. The paranthesis
  * in the name is to ensure we don't conflict with user function names.  */
