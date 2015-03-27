0074274c3cdeae92524847521e4edcea81741a0c
Benjamin Eberlei <kontakt@beberlei.de>
2015-03-27 20:48:06 +0100

    Missing includes for tests:

---
+++ tests/tideways_023_classes.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Symfony\Component\HttpKernel\Controller {
+    class ControllerResolver
+    {
+        public function createController($name)
+        {
+        }
+    }
+}
+
+namespace Zend\MVC\Controller {
+    class ControllerManager
+    {
+        public function get($name, $options = array(), $usePeeringServiceManagers = false)
+        {
+        }
+    }
+}
