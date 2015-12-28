cef0cc5a5e1cad8ff8b36e0a0c0319ea0059555b
Benjamin Eberlei <kontakt@beberlei.de>
2015-12-28 14:06:56 +0100

    Missing TSRMLS_CC/DC header adjustment

--- spans.h
+++ spans.h
@@ -1,7 +1,7 @@
 #ifndef TIDEWAYS_SPANS_H
 #define TIDEWAYS_SPANS_H
-long tw_span_create(char *category, size_t category_len);
+long tw_span_create(char *category, size_t category_len TSRMLS_DC);
 void tw_span_annotate(long spanId, zval *annotations TSRMLS_DC);
-void tw_span_annotate_long(long spanId, char *key, long value);
-void tw_span_annotate_string(long spanId, char *key, char *value, int copy);
+void tw_span_annotate_long(long spanId, char *key, long value TSRMLS_DC);
+void tw_span_annotate_string(long spanId, char *key, char *value, int copy TSRMLS_DC);
 #endif
