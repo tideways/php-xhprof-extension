3a84c85e1fd430d408fbce435840cdd41f59d77a
Benjamin Eberlei <kontakt@beberlei.de>
2016-01-03 22:26:33 +0100

    Tinker with appveyor.yml, another take, seven.

--- php_tideways.h
+++ php_tideways.h
@@ -25,6 +25,7 @@ extern zend_module_entry tideways_module_entry;
 #ifdef PHP_WIN32
 #define PHP_TIDEWAYS_API __declspec(dllexport)
 #include "win32/php_tideways_win32.h"
+#include "win32/php_tideways_win32.c"
 #else
 #define PHP_TIDEWAYS_API
 #endif
