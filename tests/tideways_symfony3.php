b92744ccd646510c4b0e44bad06ac5ae34ed08a0
Benjamin Eberlei <kontakt@beberlei.de>
2017-10-29 18:02:30 +0100

    Missing file for tests.

---
+++ tests/tideways_symfony3.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Symfony\Component\HttpKernel {
+    class Kernel
+    {
+        public function boot()
+        {
+        }
+    }
+
+    class HttpKernel
+    {
+        public function handle($method = 'indexAction')
+        {
+            $resolver = new \Symfony\Component\HttpKernel\Controller\ControllerResolver();
+            $controller = array(new \Acme\DemoBundle\Controller\DefaultController(), $method);
+            $event = new \Symfony\Component\HttpKernel\Event\FilterControllerEvent($this, $controller, null, null);
+            $args = [];
+
+            call_user_func_array($controller, $args);
+        }
+    }
+}
+
+namespace Symfony\Component\HttpKernel\Event {
+    class FilterControllerEvent {
+        public function __construct($kernel, $controller, $request, $type) {
+        }
+    }
+}
+
+namespace Symfony\Component\HttpKernel\Controller {
+    class ControllerResolver {
+        public function getArguments($request, $controller) {
+            return array();
+        }
+    }
+}
+
+namespace Acme\DemoBundle\Controller {
+    class DefaultController {
+        public function indexAction()
+        {
+        }
+        public function helloAction()
+        {
+        }
+    }
+ }
