4f4ebbbaf2347436c18725e11bf3d03d6781ba05
Benjamin Eberlei <kontakt@beberlei.de>
2019-02-08 19:46:05 +0100

    Move type definition around.

--- timer.h
+++ timer.h
@@ -11,7 +11,6 @@
 #include "win32/getrusage.h"
 #else
 
-#include "stdint.h"
 #include <sys/time.h>
 #include <sys/resource.h>
 #endif
@@ -54,6 +53,9 @@ static zend_always_inline uint64 time_milliseconds(int source, double timebase_f
     struct timespec s;
     uint32 a, d;
     uint64 val;
+#if defined(__i386__)
+    int64_t ret;
+#endif
 
     switch (source) {
 #if HAVE_CLOCK_GETTIME
@@ -77,7 +79,6 @@ static zend_always_inline uint64 time_milliseconds(int source, double timebase_f
 #endif
         case TIDEWAYS_XHPROF_CLOCK_TSC:
 #if defined(__i386__)
-            int64_t ret;
             __asm__ volatile("rdtsc" : "=A"(ret));
             return ret;
 #elif defined(__x86_64__) || defined(__amd64__)
