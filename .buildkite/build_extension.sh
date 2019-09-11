4fa960aaf7db2bec6c827fb26b385feea6123dd5
Benjamin Eberlei <kontakt@beberlei.de>
2019-09-11 11:27:59 +0200

    Fix reference to tideways.so meaning tideways_xhprof.so

--- .buildkite/build_extension.sh
+++ .buildkite/build_extension.sh
@@ -3,13 +3,14 @@
 set -e
 
 PHP_VERSION=$1
+EXTENSION="tideways_xhprof"
 
 make distclean || true
 /opt/php/php-${PHP_VERSION}/bin/phpize
 CFLAGS="-O2" ./configure --with-php-config=/opt/php/php-${PHP_VERSION}/bin/php-config
 make -j2
 
-cp modules/tideways.so modules/tideways-xhprof-${PHP_VERSION}.so
-buildkite-agent artifact upload modules/tideways-xhprof-${PHP_VERSION}.so
+cp modules/${EXTENSION}.so modules/${EXTENSION}-${PHP_VERSION}.so
+buildkite-agent artifact upload modules/${EXTENSION}-${PHP_VERSION}.so
 
-REPORT_EXIT_STATUS=1 /opt/php/php-${PHP_VERSION}/bin/php run-tests.php -p /opt/php/php-${PHP_VERSION}/bin/php --show-diff -d extension=`pwd`/.libs/tideways_xhprof.so -q
+REPORT_EXIT_STATUS=1 /opt/php/php-${PHP_VERSION}/bin/php run-tests.php -p /opt/php/php-${PHP_VERSION}/bin/php --show-diff -d extension=`pwd`/.libs/${EXTENSION}.so -q
